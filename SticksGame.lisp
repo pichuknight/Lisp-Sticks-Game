3b938f637ab89fa5c122ef10ecc6672e65a2e928
Nick Thompson <71728625+pichuknight@users.noreply.github.com>
2020-11-09 18:59:30 -0500

    Add files via upload

--- SticksGame.lisp
+++ SticksGame.lisp
@@ -40,54 +40,69 @@
 		  ;If inputer is equal to randNum or is equal to give up value, exit loop/game
 		   (or (= userIn randNum) (= userIn giveUp))))))
 
+      
 (defun cardGame()
 ;Defining variables
   (let (
-	(playerTotal 0)
-	(dealerTotal 0)
 	(hit 1)
 	(stay 2)
 	(quit 0)
 	(inc 0)
-	(win 0)
+	(win 0)			       
+	(playerIn nil)
+	(playerTotal 0)
+	(dealerTotal 0)
 	(rand1 (random 12))
 	(rand2 (random 12))
 	(rand3 (random 12))
 	(rand4 (random 12))
 	(rand5 (random 12))
 	(blackjack 21)
-	(lose 22)
-	(playerIn nil))
-	(+ playerTotal rand1)
-	(+ playerTotal rand2)
-	(+ dealerTotal rand3)
-      ; start of loop
-	(loop do (format t "Here is your total for your starting hand, and one of the dealers cards, hit: 1, stay: 2, quit:0")
-	     ;increase loop by one
-		 (incf inc)
-		 (print playerTotal)
-		 (print dealerTotal)
+	(lose 22))
+	;Initializes player total from rand 1 & 2 values
+  (incf playerTotal rand1)
+  (incf playerTotal rand2)
+	;Adds to dealer total from rand 3 values
+  (incf dealerTotal rand3)
+	;start of loop
+	(loop do (format t " Here is your total for your hand, and what you know of the dealers, hit: 1, stay: 2, quit: 0~%")
+   ;increase loop by one
+		(incf inc)
+    ;Prints out values of player and dealer start total
+    (Print "Player Total:" )
+    ;princ allows output to print on same line
+    (princ playerTotal)
+    (Print "Dealer Total:" )
+    (princ dealerTotal)
 		 (terpri)
-	      	;takes input from user
-		 (setf playerIn (read))
-		 (cond ((not (numberp playerIn)) (print "Not a number"))
-		       ;if player hits they add value
-		       ((= playerIn hit) (+ playerTotal rand4) (print playerTotal))
-		       ;if player stays their value stays the same
-			((= playerIn stay) (print playerTotal))
-			;quits game
-			((= playerIn quit) (print "Quitting game")))
-		 (+ dealerTotal rand5)
-		 (print dealerTotal)
-		 ;conditoins for the game to work
-		 (if (= playerTotal blackjack) (print "You Win"))
-		 (if (>= playerTotal lose) (print "You broke"))
-		 (if (<= playerTotal dealerTotal) (print "You lose"))
-		 (if (> playerTotal dealerTotal) (print "You win") (setq win 3))
+     ;Take userinput
+    (setf playerIn (read))
+    
+    ;Condations checking playerIn input value
+;Condations checking playerIn input value
+    (cond ((not (numberp playerIn)) (print "Not a number"))
+      ;if player hits they add value
+      ((= playerIn hit) (incf playerTotal rand4) (Print "Player Total: ") (princ playerTotal) (incf dealerTotal rand5) (print "Dealer Total: ") (princ dealerTotal))	  
+      ;if player stays their value stays the same
+      ((= playerIn stay) (Print "Player Total: ") (princ playerTotal) (incf dealerTotal rand5) (print "Dealer Total: ") (princ dealerTotal))
+      ;quits game
+((= playerIn quit) (print "Quitting game")))
+    (cond ((not (numberp playerTotal)) (print "Not a number"))
+	  
+;Player get's blackjack-21
+((= playerTotal blackjack) (print "You win!") (setf win 3))
+;Player ran out of money
+((>= playerTotal lose) (print "Your broke!"))
+;Player has more than dealer more than 21
+((<= playerTotal dealerTotal) (print "You Lose!"))
+;Player has won three times (has less than dealer)
+((> playerTotal dealerTotal) (print "You win!") (setf win 3)))
 
-	      until (and (numberp playerIn)
-			 (or (>= playerTotal dealerTotal) (= playerIn quit))
-			 (or (>= playerTotal lose) (= win 3))))))
+      ;Breaks out of loop:
+      ;Checks if value is a number
+      until (and (numberp playerIn)
+;Check list: if player quits, if player has won three times, if player has blackjack, if player is broke, if player has more than dealer.
+(or (= playerIn quit)(= win 3) (= playerTotal blackjack) (>= playerTotal lose) (<= playerTotal dealerTotal))))))
 ;gets lots of errors, working on the kinks
 
 ;Runs a text base menu to allow user to play game.
